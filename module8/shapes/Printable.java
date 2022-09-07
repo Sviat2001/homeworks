dc75527cc44a23e25ebcd885385914971f1dae22
Sviat2001 <cvcatuy@gmail.com>
2022-09-07 09:43:26 +0300

    beta

--- module8/shapes/Printable.java
+++ module8/shapes/Printable.java
@@ -1,4 +1,4 @@
-package module8.interfaces;
+package module8.shapes;
 
 public interface Printable {
     void print();
--- module8/shapes/Shape.java
+++ module8/shapes/Shape.java
@@ -1,6 +1,4 @@
 package module8.shapes;
 
-import module8.interfaces.Printable;
-
 public abstract class Shape implements Printable {
 }
