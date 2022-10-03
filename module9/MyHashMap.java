99ac7b2825bb4a56c1db8475c50e9ac6e537a166
Sviat2001 <cvcatuy@gmail.com>
2022-10-03 22:43:00 +0300

    b

--- module9/MyHashMap.java
+++ module9/MyHashMap.java
@@ -1,120 +1,116 @@
 package module9;
 
-public class MyHashMap<K, V>{
+import java.util.Arrays;
 
-    private Node<K, V> first = new Node<>(null, null, null);
-    private int mapSize = 0;
-    private static class Node<K, V> {
-        K key;
-        V value;
-        Node<K, V> next;
+public class MyHashMap<K, V> {
+    private final int SIZE = 16;
+    private final Node[] table;
+    private int currentMapSize = 0;
 
-        public Node(K key, V value, Node<K, V> next) {
-            this.key = key;
-            this.value = value;
-            this.next = next;
-        }
+    public MyHashMap() {
+        table = new Node[SIZE];
     }
 
     public void put(K key, V value) {
-        Node<K, V> buffer = first;
-        if(hasKey(key)) {
-            while(buffer.next != null) {
-                if(buffer.key.equals(key)) {
-                    buffer.value = value;
-                    mapSize++;
-                    break;
-                }
-                buffer = buffer.next;
-            }
+        int hash = key.hashCode() & (SIZE - 1);
+        Node<K, V> entry = table[hash];
 
-            if(buffer.key.equals(key)) {
-                buffer.value = value;
-                mapSize++;
-            }
-        } else if(mapSize > 0) {
-            while(buffer.next != null) {
-                buffer = buffer.next;
-            }
-
-            buffer.next = new Node<K, V>(key, value, null);
-            mapSize++;
-        } else if(mapSize == 0) {
-            first = new Node<K, V>(key, value, null);
-            mapSize++;
-        }
-    }
-
-    private boolean hasKey(K key) {
-        Node<K, V> buffer = first;
-        while(buffer.next != null) {
-            if (buffer.key.equals(key)) {
-                return true;
-            }
-            buffer = buffer.next;
-        }
-
-        if(buffer.key != null && buffer.key.equals(key)) {
-            return true;
+        if (entry == null) {
+            table[hash] = new Node<>(key, value);
         } else {
-            return false;
-        }
-    }
-
-    public void clear() {
-        first = new Node<>(null, null, null);
-        mapSize = 0;
-    }
-
-
-    public int size() {
-        return mapSize;
-    }
-
-    public void remove(K key) {
-        if(hasKey(key)) {
-            Node<K, V> buffer = first;
-            Node<K, V> prev = null;
-            while(buffer.next != null) {
-                if (buffer.key.equals(key)) {
-                    if(prev == null) {
-                        first = first.next;
-                    } else {
-                        prev.next = buffer.next;
-                    }
-
-                    mapSize--;
+            while (entry.next != null) {
+                if (entry.getKey() == key) {
+                    entry.setValue(value);
+                    return;
                 }
-                prev = buffer;
-                buffer = buffer.next;
+                entry = entry.next;
             }
 
-            if(buffer.key != null && buffer.key.equals(key)) {
-                prev.next = null;
-                mapSize--;
+            if (entry.getKey() == key) {
+                entry.setValue(value);
+                return;
             }
+            entry.next = new Node<K, V>(key, value);
         }
+        currentMapSize++;
     }
 
-    public Object get(K key) {
-        if(hasKey(key)) {
-            Node<K, V> buffer = first;
-            while(buffer.next != null) {
-                if (buffer.key.equals(key)) {
-                    return buffer.value;
-                }
+    public V get(K key) {
+        int hash = key.hashCode() & (SIZE - 1);
+        Node<K, V> entry = table[hash];
 
-                buffer = buffer.next;
-            }
-
-            if (buffer.key != null && buffer.key.equals(key)) {
-                return buffer.value;
-            }
+        if (entry == null) {
+            return null;
         }
 
+        while (entry != null) {
+            if (entry.getKey() == key) {
+                return entry.getValue();
+            }
+            entry = entry.next;
+        }
         return null;
     }
 
+    public void remove(K key) {
+        int hash = key.hashCode() % SIZE;
+        Node<K, V> entry = table[hash];
+        Node<K, V> prev = entry;
 
+        if (entry.getKey() == key) {
+            table[hash] = entry.next;
+            entry.next = null;
+        }
+
+        entry = entry.next;
+
+        while (entry != null) {
+            if (entry.getKey() == key) {
+                prev.next = entry.next;
+                entry.next = null;
+            }
+            prev = entry;
+            entry = entry.next;
+        }
+        currentMapSize--;
+    }
+
+    public void clear() {
+        Arrays.fill(table, null);
+        currentMapSize = 0;
+    }
+
+    public int size() {
+        return currentMapSize;
+    }
+
+
+    private static class Node<K, V> {
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return this.key;
+        }
+
+        public V getValue() {
+            return this.value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+
+        public void setKey(K key) {
+            this.key = key;
+        }
+    }
 
 }
